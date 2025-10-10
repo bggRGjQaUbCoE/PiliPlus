dd512f1c0ef34ae29934bf7402e217ff370ae133
My-Responsitories <107370289+My-Responsitories@users.noreply.github.com>
2025-10-10 11:16:06 +0800

    fix

--- lib/scripts/build.ps1
+++ lib/scripts/build.ps1
@@ -3,15 +3,31 @@ param(
 )
 
 try {
-    $versionName = (Get-Content pubspec.yaml | Select-String 'version:\s*([\d\.]+)').Matches[0].Groups[1].Value
+    $versionName = $null
+
+    $versionCode = [int](git rev-list --count HEAD).Trim()
+
+    $updatedContent = foreach ($line in (Get-Content -Path 'pubspec.yaml' -Encoding UTF8)) {
+        if ($line -match '^\s*version:\s*([\d\.]+)') {
+            $versionName = $matches[1]
+            "version: $versionName+$versionCode"
+        }
+        else {
+            $line
+        }
+    }
+
+    if ($null -eq $versionName) {
+        throw 'version not found'
+    }
+
+    $updatedContent | Set-Content -Path 'pubspec.yaml' -Encoding UTF8
 
     $commitHash = (git rev-parse HEAD).Trim()
 
     if ($Arg -eq 'android') {
         $versionName += '-' + $commitHash.Substring(0, 9)
     }
-
-    $versionCode = [int](git rev-list --count HEAD).Trim()
 
     $buildTime = [int]([DateTimeOffset]::Now.ToUnixTimeSeconds())
 
@@ -25,7 +41,6 @@ try {
     $data | ConvertTo-Json -Compress | Out-File 'pili_release.json' -Encoding UTF8
 
     Add-Content -Path $env:GITHUB_ENV -Value "version=$versionName"
-    Add-Content -Path $env:GITHUB_ENV -Value "number=$versionCode"
 }
 catch {
     Write-Error "Prebuild Error: $($_.Exception.Message)"

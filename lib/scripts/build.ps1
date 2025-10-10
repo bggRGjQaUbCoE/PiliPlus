57e55bc02ce5d3833500d47a01e8e8fd315a951c
bggRGjQaUbCoE <githubaccount56556@proton.me>
2025-10-10 12:41:34 +0800

    fix
    Signed-off-by: bggRGjQaUbCoE <githubaccount56556@proton.me>

--- lib/scripts/build.ps1
+++ lib/scripts/build.ps1
@@ -7,9 +7,14 @@ try {
 
     $versionCode = [int](git rev-list --count HEAD).Trim()
 
+    $commitHash = (git rev-parse HEAD).Trim()
+
     $updatedContent = foreach ($line in (Get-Content -Path 'pubspec.yaml' -Encoding UTF8)) {
         if ($line -match '^\s*version:\s*([\d\.]+)') {
             $versionName = $matches[1]
+            if ($Arg -eq 'android') {
+                $versionName += '-' + $commitHash.Substring(0, 9)
+            }
             "version: $versionName+$versionCode"
         }
         else {
@@ -23,12 +28,6 @@ try {
 
     $updatedContent | Set-Content -Path 'pubspec.yaml' -Encoding UTF8
 
-    $commitHash = (git rev-parse HEAD).Trim()
-
-    if ($Arg -eq 'android') {
-        $versionName += '-' + $commitHash.Substring(0, 9)
-    }
-
     $buildTime = [int]([DateTimeOffset]::Now.ToUnixTimeSeconds())
 
     $data = @{
@@ -40,7 +39,7 @@ try {
 
     $data | ConvertTo-Json -Compress | Out-File 'pili_release.json' -Encoding UTF8
 
-    Add-Content -Path $env:GITHUB_ENV -Value "version=$versionName"
+    Add-Content -Path $env:GITHUB_ENV -Value "version=$versionName+$versionCode"
 }
 catch {
     Write-Error "Prebuild Error: $($_.Exception.Message)"

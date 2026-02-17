c50df7f04588f2fa8dea6fbc002d1e91ecf0953b
My-Responsitories <107370289+My-Responsitories@users.noreply.github.com>
2026-02-17 19:32:16 +0800

    opt: remove addPointer

--- lib/common/widgets/gesture/image_horizontal_drag_gesture_recognizer.dart
+++ lib/common/widgets/gesture/image_horizontal_drag_gesture_recognizer.dart
@@ -1,14 +1,11 @@
 import 'package:PiliPlus/common/widgets/gesture/horizontal_drag_gesture_recognizer.dart';
-import 'package:PiliPlus/common/widgets/gesture/image_tap_gesture_recognizer.dart'
-    show ImageGestureRecognizerMixin;
 import 'package:flutter/gestures.dart';
 
 typedef IsBoundaryAllowed =
     bool Function(Offset? initialPosition, OffsetPair lastPosition);
 
 class ImageHorizontalDragGestureRecognizer
-    extends CustomHorizontalDragGestureRecognizer
-    with ImageGestureRecognizerMixin {
+    extends CustomHorizontalDragGestureRecognizer {
   ImageHorizontalDragGestureRecognizer({
     super.debugOwner,
     super.supportedDevices,

9e898e71c3ca1b254d3aa94854754b84b9a4ab7e
dom <githubaccount56556@proton.me>
2026-04-08 20:37:19 +0800

    remove unused param
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/plugin/pl_player/controller.dart
+++ lib/plugin/pl_player/controller.dart
@@ -1746,7 +1746,7 @@ class PlPlayerController with BlockConfigMixin {
     });
   }
 
-  void onPopInvokedWithResult(bool didPop, Object? result, bool isPortrait) {
+  void onPopInvokedWithResult(bool didPop, Object? result) {
     if (didPop) {
       if (Platform.isAndroid) {
         _disableAutoEnterPipIfNeeded();

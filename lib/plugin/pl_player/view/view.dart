338009d9a063b86f4c24322ea7f2b96879567c20
dom <githubaccount56556@proton.me>
2026-04-11 21:26:46 +0800

    update
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/plugin/pl_player/view/view.dart
+++ lib/plugin/pl_player/view/view.dart
@@ -38,7 +38,6 @@ import 'package:PiliPlus/pages/video/post_panel/view.dart';
 import 'package:PiliPlus/pages/video/widgets/header_control.dart';
 import 'package:PiliPlus/plugin/pl_player/controller.dart';
 import 'package:PiliPlus/plugin/pl_player/models/bottom_control_type.dart';
-import 'package:PiliPlus/plugin/pl_player/models/bottom_progress_behavior.dart';
 import 'package:PiliPlus/plugin/pl_player/models/data_status.dart';
 import 'package:PiliPlus/plugin/pl_player/models/double_tap_type.dart';
 import 'package:PiliPlus/plugin/pl_player/models/fullscreen_mode.dart';
@@ -145,7 +144,6 @@ class _PLVideoPlayerState extends State<PLVideoPlayer>
   late FullScreenMode mode;
 
   late final RxBool showRestoreScaleBtn = false.obs;
-  final RxBool _isDraggingProgress = false.obs;
 
   GestureType? _gestureType;
 
@@ -977,7 +975,6 @@ class _PLVideoPlayerState extends State<PLVideoPlayer>
       final dy = cumulativeDelta.dy.abs();
       if (dx > 3 * dy) {
         _gestureType = GestureType.horizontal;
-        _isDraggingProgress.value = true;
       } else if (dy > 3 * dx) {
         if (!plPlayerController.enableSlideVolumeBrightness &&
             !plPlayerController.enableSlideFS) {
@@ -1128,7 +1125,6 @@ class _PLVideoPlayerState extends State<PLVideoPlayer>
   }
 
   void _onInteractionEnd(ScaleEndDetails details) {
-    _isDraggingProgress.value = false;
     if (plPlayerController.showSeekPreview) {
       plPlayerController.showPreview.value = false;
     }
@@ -1276,7 +1272,6 @@ class _PLVideoPlayerState extends State<PLVideoPlayer>
     }
   }
 
-
   void _onPointerPanZoomUpdate(PointerPanZoomUpdateEvent event) {
     if (plPlayerController.controlsLock.value) return;
     if (_gestureType == null) {
@@ -1286,7 +1281,6 @@ class _PLVideoPlayerState extends State<PLVideoPlayer>
       final dy = pan.dy.abs();
       if (dx > 3 * dy) {
         _gestureType = GestureType.horizontal;
-        _isDraggingProgress.value = true;
       } else if (dy > 3 * dx) {
         _gestureType = GestureType.right;
       }
@@ -1340,7 +1334,6 @@ class _PLVideoPlayerState extends State<PLVideoPlayer>
   }
 
   void _onPointerPanZoomEnd(PointerPanZoomEndEvent event) {
-    _isDraggingProgress.value = false;
     _gestureType = null;
   }
 
@@ -1727,17 +1720,26 @@ class _PLVideoPlayerState extends State<PLVideoPlayer>
             child: Obx(
               () {
                 final showControls = plPlayerController.showControls.value;
-                final isDragging = _isDraggingProgress.value;
-                final offstage = isDragging && !showControls
-                    ? false
-                    : switch (plPlayerController.progressType) {
-                        BtmProgressBehavior.alwaysHide => true,
-                        BtmProgressBehavior.onlyShowFullScreen =>
-                          showControls || !isFullScreen,
-                        BtmProgressBehavior.onlyHideFullScreen =>
-                          showControls || isFullScreen,
-                        _ => showControls,
-                      };
+                final bool offstage;
+                switch (plPlayerController.progressType) {
+                  case .alwaysShow:
+                    offstage = showControls;
+                  case .alwaysHide:
+                    if (!plPlayerController.isSliderMoving.value) {
+                      return const SizedBox.shrink();
+                    }
+                    offstage = showControls;
+                  case .onlyShowFullScreen:
+                    offstage =
+                        showControls ||
+                        (!isFullScreen &&
+                            !plPlayerController.isSliderMoving.value);
+                  case .onlyHideFullScreen:
+                    offstage =
+                        showControls ||
+                        (isFullScreen &&
+                            !plPlayerController.isSliderMoving.value);
+                }
                 return Offstage(
                   offstage: offstage,
                   child: Stack(

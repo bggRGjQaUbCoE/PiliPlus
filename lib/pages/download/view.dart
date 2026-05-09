dc44808d6e3d51830451adc1101694c72da4ca3a
My-Responsitories <107370289+My-Responsitories@users.noreply.github.com>
2026-05-09 15:20:18 +0800

    opt: ext

--- lib/pages/download/view.dart
+++ lib/pages/download/view.dart
@@ -17,10 +17,10 @@ import 'package:PiliPlus/pages/download/detail/widgets/item.dart';
 import 'package:PiliPlus/pages/download/search/view.dart';
 import 'package:PiliPlus/services/download/download_service.dart';
 import 'package:PiliPlus/utils/cache_manager.dart';
-import 'package:PiliPlus/utils/extension/iterable_ext.dart' show IterableExt;
 import 'package:PiliPlus/utils/grid.dart';
 import 'package:PiliPlus/utils/platform_utils.dart';
 import 'package:PiliPlus/utils/storage.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart'
     hide SliverGridDelegateWithMaxCrossAxisExtent, LayoutBuilder;
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';

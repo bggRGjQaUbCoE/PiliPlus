38ff93f0950c25a41d063af881e44d19dbdfe283
dom <githubaccount56556@proton.me>
2026-06-05 10:54:22 +0800

    desktop OverscrollIndicator
    Signed-off-by: dom <githubaccount56556@proton.me>

--- lib/common/widgets/scroll_behavior.dart
+++ lib/common/widgets/scroll_behavior.dart
@@ -19,14 +19,14 @@ class CustomScrollBehavior extends MaterialScrollBehavior {
     Widget child,
     ScrollableDetails details,
   ) {
-    if (Platform.isAndroid) {
-      return StretchingOverscrollIndicator(
-        axisDirection: details.direction,
-        clipBehavior: details.decorationClipBehavior ?? .hardEdge,
-        child: child,
-      );
+    if (Platform.isIOS) {
+      return child;
     }
-    return child;
+    return StretchingOverscrollIndicator(
+      axisDirection: details.direction,
+      clipBehavior: details.decorationClipBehavior ?? .hardEdge,
+      child: child,
+    );
   }
 
   @override
